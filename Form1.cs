eb26f8370f93b140d7f3096d39403cf1d9abbf9f
Infernal15 <vlad99387@gmail.com>
2021-02-16 01:05:26 +0200

    first commit

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace synchronous_recording
+{
+    public partial class Form1 : Form
+    {
+        static List<string> list;
+        static Writer locker = new Writer();
+        public Form1()
+        {
+            InitializeComponent();
+            list = new List<string>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list = new List<string>();
+            for (int i = 0; i < richTextBox1.Text.ToString().Split('\n').Length; i++)
+            {
+                list.Add(richTextBox1.Text.ToString().Split('\n')[i]);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Thread myThread = new Thread(Array);
+                myThread.Start();
+            }
+        }
+
+        public static void Array()
+        {
+            lock (locker)
+            {
+                locker.func(list);
+            }
+        }
+    }
+}
---
+++ Writer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace synchronous_recording
+{
+    public class Writer
+    {
+        public void func(List<string> temp)
+        {
+            using (StreamWriter sw = new StreamWriter("temp.txt", false, System.Text.Encoding.UTF8))
+            {
+                for (int i = 0; i < temp.Count; i++)
+                {
+                    sw.WriteLine(temp[i]);
+                }
+            }
+        }
+    }
+}
